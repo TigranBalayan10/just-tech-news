b0a30baf2fcb2e82e05310745d81548d4bdfd633
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-03-18 14:13:40 -0700

    integrated comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
 const sequelize = require("../../config/connection");
+const { Post, User, Vote, Comment } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
+  console.log("======================");
   Post.findAll({
     attributes: [
       "id",
@@ -19,6 +20,15 @@ router.get("/", (req, res) => {
     ],
     order: [["created_at", "DESC"]],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -50,6 +60,14 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -83,7 +101,6 @@ router.post("/", (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
   Post.upvote(req.body, { Vote })
