46b7c08e4f6f5245a06511d27293297363ffedf7
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-03-21 12:33:37 -0700

    front end start

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const commentRoutes = require('./api/comment-routes');
-
-router.use('/comments', commentRoutes);
 
 router.use('/api', apiRoutes);
 
